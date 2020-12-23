2ec97689bc430ac9fe7aafe0c4bb5d794c64e69d
KarateCowboy <moriger@example.com>
2020-12-22 20:22:51 -0600

    transfer machines

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,68 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
+
+import Data.List.Split
+import Control.Lens
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+returnThree :: Int
+returnThree = 3
+
+type Name = String
+type TeamName = String
+type HP = Int
+type AP = Int
+
+data MonCmd = Attack 
+            | Choose Name
+            | Create MonPoke deriving (Show, Eq)
+
+type MonPoke = (TeamName, Name, HP, AP)
+
+data Team = Team { name :: Name, roster :: [MonPoke], defeated :: Bool }
+
+parseCmd :: String -> MonCmd
+parseCmd cmdline = 
+  let
+    wordList = splitOn " " cmdline
+    cmdWrd = head wordList
+  in
+    if cmdWrd == "CREATE" then
+      parseCreate wordList
+    else if cmdWrd == "CHOOSE" then
+      Choose (wordList !! 1)
+    else 
+      Attack
+
+parseCreate :: [String] -> MonCmd
+parseCreate words =
+  let
+    teamName = words !! 1
+    name = words !! 2
+    hp = (read (words !! 3) :: Int)
+    attack = (read (words !! 4) :: Int)
+  in
+    Create (teamName, name, hp, attack)
+
+execCmd :: MonCmd -> String
+execCmd cmd = "Meekachu has been assigned to team Rocket!"
+
+
+attacks :: MonPoke -> MonPoke -> MonPoke
+attacks attacker defender =
+  let 
+    ap = attacker ^. _4
+    defTeam = defender ^. _1
+    defName = defender ^. _2
+    defHP = defender ^. _3
+    defAP = defender ^. _4
+  in
+    (defTeam, defName, (defHP - ap), defAP)
+
+addMonToTeam :: MonPoke -> Team -> Team
+addMonToTeam newMonPoke team =
+  let
+    newRoster = newMonPoke : (roster team)
+  in
+    Team { name = (name team), defeated = (defeated team), roster = newRoster }
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,11 +1,41 @@
 import Test.Hspec
 import Test.QuickCheck
 import Control.Exception (evaluate)
+import Lib
+import Control.Lens
+
+newMeekachu :: MonPoke
+newMeekachu = ("Rocket", "Meekachu", 2, 1)
+
+newSnorfax :: MonPoke
+newSnorfax = ("Green", "Snorfax", 3,1)
+
+newRocket :: Team
+newRocket = Team { name = "Rocket", roster = [], defeated = False }
 
 main :: IO ()
 main = hspec $ do
-  describe "thing" $ do
-    it "works" $ do
-      3 `shouldBe` 3
+  describe "parseCmd" $ do
+    it "handles ATTACK" $ do
+      (parseCmd "ATTACK") `shouldBe` Attack
+    it "handles CREATE" $ do
+      (parseCmd "CREATE Rocket Meekachu 2 1") `shouldBe` Create (newMeekachu)
+    it "handles CHOOSE" $ do
+      (parseCmd "CHOOSE Meekachu") `shouldBe` Choose "Meekachu"
 
+  describe "execCmd" $ do
+    it "returns the create output when given a Create cmd" $ do
+      let createCmd = Create ("Rocket", "Meekachu", 2, 1)
+      (execCmd createCmd) `shouldBe` "Meekachu has been assigned to team Rocket!"
+  describe "attackCmd" $ do
+    it "decreases the defending Mon's HP by the attack Mon's AP" $ do
+      let whackedSnorfax = (newMeekachu `attacks` newSnorfax)
+      (whackedSnorfax ^. _3) `shouldBe` 2
+  describe "addMonToTeam" $ do
+      it "creates a mon and adds it to a team roster" $ do
+        let updatedTeam = addMonToTeam newMeekachu newRocket
+        (length (roster updatedTeam)) `shouldBe` 1
+  describe "isDefeated" $ do
+    it "says true for a team with Mon all defeated" $ do
+      1 `shouldBe` 1
 
